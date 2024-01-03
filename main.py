0005941e4a87bcd77f8370da33450ea0684c6e60
Alekc <alekc.genom@yandex.ru>
2024-01-03 12:27:22 +0000

    add compose_file and edit main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ cursor.execute(create_table_query)
 @app.route('/')
 def index():
     # Получение IP-адреса пользователя
-    ip_address = request.headers.get('X-Forwarded-For')
+    ip_address = request.environ['REMOTE_ADDR'] #request.headers.get('X-FORWARDED-FOR')
 
     # Запись в базу данных
     now = datetime.now()
